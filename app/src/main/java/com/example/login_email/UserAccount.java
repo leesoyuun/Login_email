d22cab70dc9b22bed4fb68196b4136ffd10aff95
leesoyun <leesoyun4206@naver.com>
2021-05-02 01:39:17 +0900

    login & register ( with email and firebase)

---
+++ app/src/main/java/com/example/login_email/UserAccount.java
@@ -0,0 +1,37 @@
+package com.example.login_email;
+/*
+사용자 계정 정보 모델 클래스
+*/
+
+public class UserAccount
+{
+    private String idToken; // Firebase Uid( 고유토큰정보)
+    private String emailId;
+    private String password;
+
+    public UserAccount() {    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
